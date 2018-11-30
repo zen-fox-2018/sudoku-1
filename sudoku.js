7f0036f5076529c24cd761a8543d25a10dabf202
MuhammadRamadiansyah <ramadiansyah@hotmail.com>
2018-11-30 15:22:08 +0700

    Template

---
+++ sudoku.js
@@ -0,0 +1,24 @@
+"use strict"
+
+class Sudoku {
+  constructor(board_string) {}
+
+  solve() {}
+
+  // Returns a string representing the current state of the board
+  board() {}
+}
+
+// The file has newlines at the end of each line,
+// so we call split to remove it (\n)
+var fs = require('fs')
+var board_string = fs.readFileSync('set-01_sample.unsolved.txt')
+  .toString()
+  .split("\n")[0]
+
+var game = new Sudoku(board_string)
+
+// Remember: this will just fill out what it can and not "guess"
+game.solve()
+
+console.log(game.board())
