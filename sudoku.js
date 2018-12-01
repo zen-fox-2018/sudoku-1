a2dfc67f208e0d3a8ff1d2f0b172335d892c2607
maitreya <bluespider529@gmail.com>
2018-12-01 12:48:05 +0700

    check grid

--- sudoku.js
+++ sudoku.js
@@ -1,14 +1,14 @@
 "use strict"
 
 class Sudoku {
+
   constructor(board_string) {
     this.boardGame = board_string;
     this.coordinates = [];
+    this.sudokuBoard = this.board()
   }
 
-  solve() {
-
-  }
+  solve() {}
 
   // Returns a string representing the current state of the board
   board() {
@@ -29,11 +29,9 @@ class Sudoku {
 
   generateCoordinates() {
 
-    let sudokuBoard = this.board();
-
-    for(let i = 0; i < sudokuBoard.length; i++) {
-      for(let j = 0; j < sudokuBoard[i].length; j++) {
-        if (sudokuBoard[i][j] === 0) {
+    for(let i = 0; i < this.sudokuBoard.length; i++) {
+      for(let j = 0; j < this.sudokuBoard[i].length; j++) {
+        if (this.sudokuBoard[i][j] === 0) {
           let sudokuCoor = {
             row: i,
             column: j,
@@ -47,10 +45,8 @@ class Sudoku {
 
   checkHorizontal(row, number) {
 
-    let sudokuBoard = this.board();
-
-    for(let i = 0; i < sudokuBoard.length - 1; i++) {
-      if(sudokuBoard[row][i] === number) { //---> horizontal
+    for(let i = 0; i < this.sudokuBoard.length - 1; i++) {
+      if(this.sudokuBoard[row][i] === number) { //---> horizontal
         return false
       }
     }
@@ -59,12 +55,30 @@ class Sudoku {
 
   checkVertical(column, number) {
 
-    let sudokuBoard = this.board();
-
-    for(let i = 0; i < sudokuBoard.length - 1; i++) {
+    for(let i = 0; i < this.sudokuBoard.length - 1; i++) {
       // console.log(sudokuBoard[i][0]) ---> vertical!
-      if(sudokuBoard[i][column] === number) {
+      if(this.sudokuBoard[i][column] === number) {
         return false
+      }
+    }
+    return true
+  }
+
+  checkGrid(row, column, number) {
+    
+    //baris
+    let startRow = Math.floor(row/3) * 3;
+    let endRow = startRow + 2;
+
+    //kolom
+    let startCol = Math.floor(column/3) * 3;
+    let endCol = startCol + 2;
+
+    for(let i = startRow; i < endRow; i++) {
+      for(let j = startCol; j < endCol; j++) {
+        if (this.sudokuBoard[i][j] === number) {
+          return false
+        }
       }
     }
     return true
@@ -81,9 +95,9 @@ var board_string = fs.readFileSync('set-01_sample.unsolved.txt')
 var game = new Sudoku(board_string)
 
 // Remember: this will just fill out what it can and not "guess"
-// game.solve();
 game.generateCoordinates();
-
-console.log(game.checkHorizontal(3, 6));
-console.log(game.checkVertical(0, 2))
+// console.log(game.solve())
+// console.log(game.checkGrid(0, 1, 2));
+// console.log(game.checkHorizontal(0, 2));
+// console.log(game.checkVertical(0, 2))
 // game.checkVertical();
