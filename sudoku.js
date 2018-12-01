764501f0e65c54a2babded26ca0e5c28479420d0
rachmadza <razato.zai@gmail.com>
2018-12-01 14:00:08 +0700

    update push anchor sudoku

--- sudoku.js
+++ sudoku.js
@@ -1,100 +1,124 @@
+//mohon koreksinya kakak. terima kasih :smile: 
+
+"use strict"
+
 class Sudoku {
-  constructor(board_string) {
-    this.board_string=board_string
-  }
+    constructor(board_string) {
+        this.papan = board_string
+        this.board = this.board()
+        this.posisiNol = []
 
+    }
+
+
+    // Returns a string representing the current state of the board
     board() {
-        var printBoard=[]
-        var index=0
-        for(let i=0; i<9; i++){
-        var smallBoard=[]
-            for(let j=0; j<9; j++){
-                smallBoard.push(Number(this.board_string[index]))
-                index++
+        let papanSudoku = []
+        let counterIndex = 0
+
+        for (let i = 0; i < 9; i++) {
+            let box = []
+            for (let j = 0; j < 9; j++) {
+                box.push(+this.papan[counterIndex])
+                counterIndex++
             }
-            printBoard.push(smallBoard)
+            papanSudoku.push(box)
         }
-        return printBoard
+        return papanSudoku
     }
 
-    checkCol(col, index, num){
-        let colBoard = [];
-        let arrCol = (pos, index) => pos.map(position => position[index])
-        colBoard = arrCol(col, index)
-        if (colBoard.indexOf(num) !== -1) {
-            return false
-        }
-        return true
-    }
-
-    checkRow(row, num){
-        if (row.indexOf(num) !== -1) {
-            return false
-        }
-        return true
-    }
-
-    checkSquare(board, positionX, positionY, num){ //check square3x3
-        let currentX = Math.floor(positionX / 3) * 3
-        let currentY = Math.floor(positionY / 3) * 3
-        for (let i = currentX; i < currentX+3; i++){
-            for (let j = currentY; j < currentY+3; j++){
-                if (board[i][j] === num){
+    cekH(x, y, value) {
+        for (let i = 0; i < this.board[x].length; i++) {
+            if (i !== y) {
+                if (this.board[x][i] === value) {
                     return false
                 }
             }
         }
         return true
     }
-
-    solve(board){
-        var start = 1
-        var end = 9
-        var backTrack = []
-        for (let i = 0; i < board.length; i++){
-            for (let j = 0; j < board[i].length; j++){
-                var isUnique = false
-                if (board[i][j] === 0){
-                    for (let x = start; x <= end; x++) {
-                        if (this.checkRow(board[i], x)
-                            && this.checkCol(board, j, x)
-                            && this.checkSquare(board, i, j, x)){
-                                board[i][j] = x
-                                backTrack.push([i,j]) // bntuk jd nested array
-                                isUnique = true
-                                start = 1
-                                break
-                        }
-                    }
-
-                    if (isUnique === false){
-                        var endCoor = backTrack.slice(-1)[0] // ambil arr backtrack yg pling akhir
-                        backTrack.splice(backTrack.length-1) // ambil all array backtrack kecuali yg pling trakhir 
-                        start = board[endCoor[0]][endCoor[1]] + 1 // start dimulai dari hasil index coordinator trakhir plus 1
-                        i = endCoor[0]
-                        j = endCoor[1] - 1
-                        board[endCoor[0]][endCoor[1]] = 0
-                    }
+    
+    cekV(x, y, value) {
+        for (let i = 0; i < this.board[x].length; i++) {
+            if (i !== x) {
+                if (this.board[x][i] === value) {
+                    return false
                 }
             }
         }
-        return board
+        return true
     }
+    
+    cekGrid(x, y, num){ //check square3x3
+        let currentX = Math.floor(x / 3) * 3
+        let currentY = Math.floor(y / 3) * 3
+        for (let i = currentX; i < currentX+3; i++){
+            for (let j = currentY; j < currentY+3; j++){
+                if (this.board[i][j] === num){
+                    return false
+                }
+            }
+        }
+        return true
+    }
+    
+    
+    cekNol() {
+        for (let i = 0; i < this.board.length; i++) {
+            for (let j = 0; j < this.board[i].length; j++) {
+                let koor = {
+                    x: 0,
+                    y: 0
+                }
+                
+                if (this.board[i][j] == 0) {
+                    koor.x = i
+                    koor.y = j
+                    this.posisiNol.push(koor)
+                }
+            }
+        }
+        return this.posisiNol
+    }
+    
+    solve() { 
+    
+    }
+
+    
 }
+    
+
+
+
 
 // The file has newlines at the end of each line,
 // so we call split to remove it (\n)
 var fs = require('fs')
 var board_string = fs.readFileSync('set-01_sample.unsolved.txt')
-  .toString()
-  .split("\n")[0]
+    .toString()
+    .split("\n")[0]
 
 var game = new Sudoku(board_string)
-// Remember: this will just fill out what it can and not "guess"
-game.board()
-console.log("Before:")
-console.log(game.board())
-console.log("============================")
 
-console.log("After:")
-console.log(game.solve(game.board()))
+// Remember: this will just fill out what it can and not "guess"
+// game.solve()
+
+// console.log(game.board())
+// console.log('ini papan ===> ' + '\n',game.board())
+// console.log('ini object game ===>',game)
+
+console.log('ini cek Ho',game.cekH(0, 1, 3))
+
+console.log('ini cek Ve',game.cekV(1, 3, 3))
+
+console.log('ini cek Gr',game.cekGrid(0, 2, 3));
+
+console.log(game.board)
+
+
+
+
+// console.log(game.cekNol())
+
+
